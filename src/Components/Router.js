64a6b558f9d43e2192fa7f02a5f765ff212d7573
yjkim <ureal4527@naver.com>
2021-01-12 01:08:36 +0900

    test

---
+++ src/Components/Router.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {HashRouter as Router, Route} from "react-router-dom";
+import Home from "Routes/Home";
+export default () => {
+    <Router>
+        <Route path="/" exact component={Home} />    
+    </Router>
+}
