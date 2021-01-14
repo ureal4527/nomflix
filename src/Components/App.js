87463ee8305265a39ce951b4ef21dab6abe30e15
yjkim <ureal4527@naver.com>
2021-01-14 23:53:51 +0900

    2021/01/14

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Router from "Components/Router";
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return(
       <>
--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import {HashRouter as Router, Route} from "react-router-dom";
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Home from "Routes/Home";
-export default () => {
-    <Router>
-        <Route path="/" exact component={Home} />    
-    </Router>
-}
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+export default () => (
+  <Router>
+    <>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+    </>
+  </Router>
+);
