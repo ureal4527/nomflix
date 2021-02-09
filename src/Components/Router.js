4cab4d301c8808f70b92719006e51c93f4f6833b
yjkim <ureal4527@naver.com>
2021-02-09 23:28:39 +0900

    styles

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -3,15 +3,17 @@ import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-d
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
-
+import Header from "Components/Header"
 export default () => (
   <Router>
+    <>
+    <Header/>
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/tv" exact component={TV} />
       <Route path="/search" exact component={Search} />
-      
       <Redirect from ="*" to= "/" />
     </Switch>
+    </>
   </Router>
 );
